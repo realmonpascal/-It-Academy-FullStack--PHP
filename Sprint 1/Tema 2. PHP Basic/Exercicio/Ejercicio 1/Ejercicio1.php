f437f4a52279cc41ba3c31131fdffc1c918d963f
realmonpascal <realmonpascal@gmail.com>
2025-03-20 14:27:59 +0100

    ejercicio1 de PHP

---
+++ Sprint 1/Tema 2. PHP Basic/Exercicio/Ejercicio 1/Ejercicio1.php
@@ -0,0 +1,9 @@
+<?php
+$int = 7;
+$double = 5.5;
+$String = "Pedro";
+$boolean = $int=7;
+
+echo "El integer es : ".$int ." El double es: ". $double ." El String es: " .$String . " El boolean es: " . $boolean;
+
+?>
